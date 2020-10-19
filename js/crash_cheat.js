60897a07d58a147388a11ccbc824fa8e3410f74d
weiyuexin <3022422894@qq.com>
2020-10-20 01:26:26 +0800

    Site updated: 2020-10-20 01:26:26

---
+++ js/crash_cheat.js
@@ -0,0 +1,17 @@
+<!--崩溃欺骗-->
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/joke.ico");
+         document.title = '！！这里这里,出问题了哦 ◕ ں ◕ ';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/favicon.ico");
+         document.title = '(ฅ>ω<*ฅ) 又好了,爱你哟~' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 3000);
+     }
+ });
